ed727fc04cf32f778c49e909b3100955cdca3760
trung-tran-sts <trung.tran@saigontechnology.com>
2022-11-11 13:49:32 +0700

    Add project files.

---
+++ SolarFarm.IotDataHandler/Program.cs
@@ -0,0 +1,10 @@
+using SolarFarm.IotDataHandler;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
